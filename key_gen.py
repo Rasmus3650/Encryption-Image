e581095e3f2c9e6dd5c69221cec5e6daa5d347b3
rasmus3650 <rasmus3650@gmail.com>
2019-09-10 10:37:05 +0200

    key gen

--- key_gen.py
+++ key_gen.py
@@ -3,9 +3,9 @@ import string
 
 def key_gen():
     user_key = ''
-    for i in range(random.randint(4,8)):
+    for i in range(random.randint(9,17)):
         char = random.randint(0,51)
         partial_key = string.ascii_letters[char]
         user_key += user_key.join(partial_key)
+    user_key = user_key.lower()
     return user_key
-
